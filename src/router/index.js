17eb4973422f3e187210445d87178ca15ca4918e
yimeiyue <982207333@qq.com>
2019-10-26 00:04:23 +0800

    使用vue-router

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  // 路由配置对象
+  // 关键选项：routes 作用：配置路由规则
+  routes: []
+})
+export default router
